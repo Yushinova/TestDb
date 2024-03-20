9198acfdd4ca38b7166b95d9a6b82cbeef609616
User <User@DESKTOP-H7HCK7B>
2024-03-20 23:37:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data;
+
+var conectionstring = @"Data Source=gg.db;";
+
+using var db = new SqliteConnection(conectionstring);
+db.Open();
+var command = new SqliteCommand()
+{
+    Connection = db,
+    CommandText = "select * from table_persons"
+};
+using var result = command.ExecuteReader();
+while (result.Read())
+{
+    Console.WriteLine(result.GetInt32("ID"));//мы можем указать наименование полей таблицы
+    Console.WriteLine(result.GetString(1));
+    Console.WriteLine(result.GetString(2));
+    Console.WriteLine(result.GetDateTime(3).ToString("d"));
+}
